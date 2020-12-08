cf7a642c122c61408e615f62b3f621c76c96480d
Wouter van den Brink <git@woutervdb.com>
2020-12-08 21:00:38 +0100

    Add 2015 day 8

---
+++ src/Puzzles/Year2015/Day08.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Knevelina\AdventOfCode\Puzzles\Year2015;
+
+use JetBrains\PhpStorm\Pure;
+use Knevelina\AdventOfCode\Contracts\PuzzleSolver;
+use Knevelina\AdventOfCode\InputManipulator;
+
+class Day08 implements PuzzleSolver
+{
+
+    #[Pure] public static function getStringLength(string $string): int
+    {
+        $string = preg_replace('/\\\\(x[a-f0-9]{2}|\\\\|")/', '_', substr($string, 1, -1));
+
+        return strlen($string);
+    }
+
+    #[Pure] public static function getEncodedLength(string $string): int
+    {
+        $string = preg_replace('/\\\\x[a-f0-9]{2}/', '__x49', $string);
+        $string = str_replace('"', '_"', $string);
+        $string = str_replace('\\', '__', $string);
+
+        $string = '"'.$string.'"';
+
+        return strlen($string);
+    }
+
+    public function part1(string $input): int
+    {
+        return collect(InputManipulator::splitLines($input))
+            ->map(fn (string $string): int => strlen($string) - self::getStringLength($string))
+            ->sum();
+    }
+
+    public function part2(string $input): int
+    {
+        return collect(InputManipulator::splitLines($input))
+            ->map(fn (string $string): int => self::getEncodedLength($string) - strlen($string))
+            ->sum();
+    }
+}
---
+++ tests/Puzzles/Year2015/Day08Test.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Knevelina\AdventOfCode\Tests\Puzzles\Year2015;
+
+use Knevelina\AdventOfCode\Puzzles\Year2015\Day08;
+use Knevelina\AdventOfCode\Tests\PuzzleSolverTestCase;
+
+class Day08Test extends PuzzleSolverTestCase
+{
+
+    public function regularStrings(): array
+    {
+        return [
+            ['""', 0, '"\\"\\""'],
+            ['"abc"', 3, '"\\"abc\\""'],
+            ['"abcde"', 5, '"\\"abcde\\""'],
+            ['"abcdefg"', 7, '"\\"abcdefg\\""'],
+            ['"abc\\defg"', 8, '"\\"abc\\\\defg\\""']
+        ];
+    }
+
+    public function stringsWithDoubleQuoteCharacters(): array
+    {
+        return [
+            ['"\\""', 1, '"\"\\\\\\"\\""'],             // "\""     \"\\\"\"
+            ['"a\\""', 2, '"\\"a\\\\\\"\""'],           // "a\""    \"a\\\"\"
+            ['"\\"a"', 2, '"\\"\\\\\\"a\\""'],          // "\"a"    \"\\\"a\"
+            ['"\\"\\""', 2, '"\\"\\\\\\"\\\\\\"\\""'],  // "\"\""   \"\\\"\\\"\"
+            ['"\\"a\\""', 3, '"\\"\\\\\\"a\\\\\\"\\""'] // "\"a\""  \"\\\"a\\\"\"
+        ];
+    }
+    
+    public function stringsWithBackslashes(): array
+    {
+        return [
+            ['"\\\\"', 1, '"\\"\\\\\\\\\\""'],      // "\\" \"\\\\\"
+            ['"\\\\\\\\"', 2, '"\\"\\\\\\\\\\\\\\\\\\""'],  // "\\\\" \"\\\\\\\\\"
+            ['"a\\\\"', 2, '"\\"a\\\\\\\\\\""'],     // "a\\" \"a\\\\\"
+            ['"\\\\a"', 2, '"\\"\\\\\\\\a\\""'],     // "\\a" \"\\\\a\"
+            ['"\\\\a\\\\"', 3, '"\\"\\\\\\\\a\\\\\\\\\\""']  // "\\a\\" \"\\\\a\\\\\"
+        ];
+    }
+    
+    public function stringsWithHexadecimalASCIICodes(): array
+    {
+        return [
+            ['"\\x01"', 1],
+            ['"\\xaa"', 2],
+            ['"\\x1a"', 1],
+            ['"\\xaz"', 4]
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider regularStrings
+     * @dataProvider stringsWithDoubleQuoteCharacters
+     * @dataProvider stringsWithBackslashes
+     */
+    function it_calculates_string_lengths_and_encodes_strings(string $codeString, int $length, string $encoded): void
+    {
+        $this->assertEquals($length, Day08::getStringLength($codeString));
+
+        $this->assertEquals(strlen($encoded), Day08::getEncodedLength($codeString));
+    }
+
+    public function getExamples(): array
+    {
+        return [
+            [1, 1, 12],
+            [1, 2, 19]
+        ];
+    }
+
+    public function getSolutionForPart1(): int
+    {
+        return 1350;
+    }
+
+    public function getSolutionForPart2(): int
+    {
+        return 2085;
+    }
+}
