8d9101a319122412345a62d79358f89bb845759c
Wouter van den Brink <git@woutervdb.com>
2020-12-15 16:22:25 +0100

    Remove dead code

--- src/Puzzles/Year2020/Day15.php
+++ src/Puzzles/Year2020/Day15.php
@@ -6,44 +6,6 @@ use Knevelina\AdventOfCode\Contracts\PuzzleSolver;
 
 class Day15 implements PuzzleSolver
 {
-    private array $say;
-    private array $history;
-
-    private function say(int $turn, int $num): void
-    {
-        $this->say[$turn] = $num;
-
-        if (!isset($this->history[$num])) {
-            $this->history[$num] = [];
-        }
-
-        $this->history[$num][] = $turn;
-
-        if (count($this->history[$num]) === 3) {
-            array_shift($this->history[$num]);
-        }
-    }
-
-    private function lastSaidBefore(int $turn): bool
-    {
-        if (!isset($this->say[$turn - 1])) {
-            return false;
-        }
-
-        $last = $this->say[$turn - 1];
-
-        return count($this->history[$last] ?? []) > 1;
-    }
-
-    private function getLastTwoTurns(int $turn): array
-    {
-        $last = $this->say[$turn - 1];
-
-        $history = $this->history[$last];
-
-        return [$history[count($history) - 2], $history[count($history) - 1]];
-    }
-
     private function calculate(string $input, int $n): int
     {
         $input = explode(',', trim($input));
