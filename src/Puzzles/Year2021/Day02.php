b1241962cb98461cf8240a4c93d6ff960c2e8383
Wouter van den Brink <git@woutervdb.com>
2021-12-02 13:49:00 +0100

    Add 2021 day 2

---
+++ src/Puzzles/Year2021/Day02.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Knevelina\AdventOfCode\Puzzles\Year2021;
+
+use Knevelina\AdventOfCode\Contracts\PuzzleSolver;
+use Knevelina\AdventOfCode\InputManipulator;
+
+class Day02 implements PuzzleSolver
+{
+    private static function parseInstruction(string $line): object
+    {
+        list($direction, $distance) = explode(' ', $line, 2);
+
+        return (object)[
+            'direction' => $direction,
+            'distance' => intval($distance)
+        ];
+    }
+    private static function parseInstructions(string $input): array
+    {
+        $input = InputManipulator::splitLines($input);
+
+        return array_map([static::class, 'parseInstruction'], $input);
+    }
+    public function part1(string $input): int
+    {
+        $instructions = self::parseInstructions($input);
+
+        $horizontal = 0;
+        $depth = 0;
+
+        foreach ($instructions as $instruction) {
+            $horizontal += match($instruction->direction) {
+                'forward' => $instruction->distance,
+                default => 0
+            };
+
+            $depth += match($instruction->direction) {
+                'down' => $instruction->distance,
+                'up' => -$instruction->distance,
+                default => 0
+            };
+        }
+
+        return $horizontal * $depth;
+    }
+
+    public function part2(string $input): int
+    {
+        $instructions = self::parseInstructions($input);
+
+        $horizontal = 0;
+        $aim = 0;
+        $depth = 0;
+
+        foreach ($instructions as $instruction) {
+            switch ($instruction->direction) {
+                case 'down':
+                    $aim += $instruction->distance;
+                    break;
+                case 'up':
+                    $aim -= $instruction->distance;
+                    break;
+                case 'forward':
+                    $horizontal += $instruction->distance;
+                    $depth += $aim * $instruction->distance;
+                    break;
+            }
+        }
+
+        return $horizontal * $depth;
+    }
+}
---
+++ tests/Puzzles/Year2021/Day02Test.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Knevelina\AdventOfCode\Tests\Puzzles\Year2021;
+
+use Knevelina\AdventOfCode\Tests\PuzzleSolverTestCase;
+
+/**
+ * @covers \Knevelina\AdventOfCode\Puzzles\Year2021\Day02
+ */
+class Day02Test extends PuzzleSolverTestCase
+{
+    public function getExamples(): array
+    {
+        return [
+            [1, 1, 150],
+            [1, 2, 900]
+        ];
+    }
+}
