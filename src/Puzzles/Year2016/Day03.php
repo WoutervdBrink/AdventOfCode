5da7d31bb5db51bda6e2a795396eb0925d8fa16a
Wouter van den Brink <git@woutervdb.com>
2022-12-05 19:40:54 +0100

    Add 2016 day 3

---
+++ src/Puzzles/Year2016/Day03.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Knevelina\AdventOfCode\Puzzles\Year2016;
+
+use Knevelina\AdventOfCode\Contracts\PuzzleSolver;
+use Knevelina\AdventOfCode\InputManipulator;
+
+class Day03 implements PuzzleSolver
+{
+    private static function parseInput(string $input): array
+    {
+        return InputManipulator::splitLines($input, manipulator: function (string $line): array {
+            $sides = [];
+
+            $line = preg_split('/ +/', $line);
+
+            foreach ($line as $side) {
+                $side = intval($side);
+
+                if ($side > 0) {
+                    $sides[] = $side;
+                }
+            }
+
+            return $sides;
+        });
+    }
+
+    private static function isValidTriangle(array $triangle): bool
+    {
+        sort($triangle);
+        return $triangle[0] + $triangle[1] > $triangle[2];
+    }
+
+    public function part1(string $input): int
+    {
+        return array_reduce(
+            self::parseInput($input),
+            fn (int $total, array $triangle) => $total + (self::isValidTriangle($triangle) ? 1 : 0),
+            0
+        );
+    }
+
+    public function part2(string $input): int
+    {
+        $input = self::parseInput($input);
+
+        $triangles = [];
+
+        for ($i = 0; $i < count($input) / 3; $i++) {
+            $offset = $i * 3;
+            for ($c = 0; $c < 3; $c++) {
+                $triangles[] = [$input[$offset][$c], $input[$offset + 1][$c], $input[$offset + 2][$c]];
+            }
+        }
+
+        return array_reduce(
+            $triangles,
+            fn (int $total, array $triangle) => $total + (self::isValidTriangle($triangle) ? 1 : 0),
+            0
+        );
+    }
+}
---
+++ tests/Puzzles/Year2016/Day03Test.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Knevelina\AdventOfCode\Tests\Puzzles\Year2016;
+
+use Knevelina\AdventOfCode\Tests\PuzzleSolverTestCase;
+
+/**
+ * @covers \Knevelina\AdventOfCode\Puzzles\Year2016\Day03
+ */
+class Day03Test extends PuzzleSolverTestCase
+{
+    public function getExamples(): array
+    {
+        return [
+            [1, 1, 0],
+            [2, 2, 0],
+        ];
+    }
+
+    public function getSolutionForPart1(): int
+    {
+        return 1032;
+    }
+}
