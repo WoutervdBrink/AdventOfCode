bc427e51fe70dabf155087cc175f5b34abc095b8
Wouter van den Brink <git@woutervdb.com>
2022-12-07 01:36:33 +0100

    Fix test

--- tests/Puzzles/Year2016/Day03Test.php
+++ tests/Puzzles/Year2016/Day03Test.php
@@ -13,12 +13,17 @@ class Day03Test extends PuzzleSolverTestCase
     {
         return [
             [1, 1, 0],
-            [2, 2, 0],
+            [2, 2, 6],
         ];
     }
 
     public function getSolutionForPart1(): int
     {
         return 1032;
+    }
+
+    public function getSolutionForPart2(): int
+    {
+        return 1838;
     }
 }
