25c2e363f0e56f7b4f305419526fbf26ad499214
Wouter van den Brink <git@woutervdb.com>
2022-12-07 17:08:31 +0100

    Add defaults to creation script

--- create.php
+++ create.php
@@ -9,15 +9,17 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
-if ($argc < 3) {
-    echo 'Usage: php ' . $argv[0] . ' YEAR DAY' . PHP_EOL;
-    exit(1);
+$year = intval(date('Y'));
+$day = intval(date('j'));
+
+if ($argc === 3) {
+    $year = intval($argv[1]);
+    $day = intval($argv[2]);
+} elseif ($argc === 2) {
+    $day = intval($argv[1]);
 }
 
 try {
-    $year = intval($argv[1]);
-    $day = intval($argv[2]);
-
     PuzzleSolverCreator::createPuzzle($year, $day);
 
     if (!empty($_ENV['SESSION_COOKIE'])) {
