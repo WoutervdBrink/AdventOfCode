913b6aa2ed066a17d4a950a5bfa8a73cbbe47884
Wouter van den Brink <git@woutervdb.com>
2024-12-03 14:32:08 +0100

    Add semi-linear variant of 2024 day 3

--- src/Puzzles/Year2024/Day03.php
+++ src/Puzzles/Year2024/Day03.php
@@ -6,38 +6,72 @@ use Knevelina\AdventOfCode\Contracts\PuzzleSolver;
 
 class Day03 implements PuzzleSolver
 {
+    /**
+     * @param string $input
+     * @return list<int>
+     */
+    private static function solve(string $input): array
+    {
+        $cursor = 0;
+        $factor = 1;
+        $part1 = 0;
+        $part2 = 0;
+
+        while ($cursor < strlen($input)) {
+            $peek = substr($input, $cursor, 7);
+
+            if (str_starts_with($peek, 'mul(')) {
+                $cursor += 4;
+            } else if (str_starts_with($peek, 'do()')) {
+                $cursor += 4;
+                $factor = 1;
+                continue;
+            } else if ($peek === 'don\'t()') {
+                $factor = 0;
+                $cursor += 7;
+                continue;
+            } else {
+                $cursor += 1;
+                continue;
+            }
+
+            $num1 = 0;
+            $num2 = 0;
+
+            while (is_numeric($char = $input[$cursor])) {
+                $num1 = 10 * $num1 + intval($char);
+                $cursor++;
+            }
+
+            if ($input[$cursor] !== ',') {
+                continue;
+            }
+            $cursor++;
+
+            while (is_numeric($char = $input[$cursor])) {
+                $num2 = 10 * $num2 + intval($char);
+                $cursor++;
+            }
+
+            if ($input[$cursor] !== ')') {
+                continue;
+            }
+            $cursor++;
+
+            $part1 += $num1 * $num2;
+            $part2 += $num1 * $num2 * $factor;
+        }
+
+        return [$part1, $part2];
+    }
+
     public function part1(string $input): int
     {
-        preg_match_all('/mul\\((\d{1,3}),(\d{1,3})\\)/', $input, $matches, PREG_SET_ORDER);
-        return array_reduce(
-            $matches,
-            fn(int $carry, array $match): int => $carry + (int)$match[1] * (int)$match[2],
-            0
-        );
+        return self::solve($input)[0];
     }
 
     public function part2(string $input): int
     {
-        preg_match_all('/(mul)\\((\d{1,3}),(\d{1,3})\\)|(do)\\(\\)|(don\'t)\\(\\)/', $input, $matches, PREG_SET_ORDER);
-
-        $doing = true;
-        $sum = 0;
-
-        foreach ($matches as $match) {
-            switch ($match[0]) {
-                case 'do()':
-                    $doing = true;
-                    break;
-                case 'don\'t()':
-                    $doing = false;
-                    break;
-                default:
-                    if ($doing) {
-                        $sum += (int)$match[2] * (int)$match[3];
-                    }
-            }
-        }
-
-        return $sum;
+        return self::solve($input)[1];
     }
 }
